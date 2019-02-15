a2b63a5d0a3eaf818572c963bc428b1f3853f4d0
ndmug261 <ndmug261@learner.dvusd.org>
2019-02-15 11:34:53 -0700

    Added Menu

--- BookManager/Form1.Designer.vb
+++ BookManager/Form1.Designer.vb
@@ -22,10 +22,59 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.LoginToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RegisterToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.IHaveAnAccountToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LoginToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'LoginToolStripMenuItem
+        '
+        Me.LoginToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RegisterToolStripMenuItem, Me.IHaveAnAccountToolStripMenuItem})
+        Me.LoginToolStripMenuItem.Name = "LoginToolStripMenuItem"
+        Me.LoginToolStripMenuItem.Size = New System.Drawing.Size(49, 20)
+        Me.LoginToolStripMenuItem.Text = "&Login"
+        '
+        'RegisterToolStripMenuItem
+        '
+        Me.RegisterToolStripMenuItem.Name = "RegisterToolStripMenuItem"
+        Me.RegisterToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.RegisterToolStripMenuItem.Text = "&Register"
+        '
+        'IHaveAnAccountToolStripMenuItem
+        '
+        Me.IHaveAnAccountToolStripMenuItem.Name = "IHaveAnAccountToolStripMenuItem"
+        Me.IHaveAnAccountToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.IHaveAnAccountToolStripMenuItem.Text = "&I have an account"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents LoginToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RegisterToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents IHaveAnAccountToolStripMenuItem As ToolStripMenuItem
 End Class
